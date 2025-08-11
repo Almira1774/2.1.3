0d30051999be93b16470d4bc8c31874ff38d1394
Almira Vasiluk <bezzbashennaya@gmail.com>
2025-08-11 16:41:50 +0300

    привела в порядок мобилку

--- Select.js
+++ Select.js
@@ -1,40 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper(".swiper", {
-        slidesPerView: 1.3, // По умолчанию 1 слайд
-      spaceBetween: 2,
-      slidesPerGroup: 1,
-     
-     
-       // Отступы между слайдами
-      loop: true,
-       // Зацикливание слайдов
-      /*autoplay: {
-        delay: 5000, // Автопрокрутка каждые 5 секунд
-        disableOnInteraction: false, // Автопрокрутка не останавливается при взаимодействии
-      },*/
-      
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      effect: "fade", // Плавное появление нового слайда
-      fadeEffect: {
-        crossFade: true,
-      },
-      on: {
-        init: function () {
-          // Показываем контент на первом активном слайде
-          document
-            .querySelectorAll(".swiper-slide-active .slide-content")
-            .forEach((el) => {
-              el.style.opacity = "1";
-              el.style.transform = "translateY(0)";
-            });
+        slidesPerView: 1.2, // По умолчанию 1 слайд
+        spaceBetween: 3.67,
+        slidesPerGroup: 1,
+        centeredSlides: false, // Центрирование активного слайда
+       loop:true,
+
+        // Отступы между слайдами
+
+        // Зацикливание слайдов
+        /*autoplay: {
+          delay: 5000, // Автопрокрутка каждые 5 секунд
+          disableOnInteraction: false, // Автопрокрутка не останавливается при взаимодействии
+        },*/
+
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
-      },
+        effect: 'coverflow',
+       coverflowEffect: {
+          slideShadows: true,
+          
+        modifier: 0,
+         
+          
+
+        },
+        on: {
+            init: function () {
+                // Показываем контент на первом активном слайде
+                document
+                    .querySelectorAll(".swiper-slide-active .slide-content")
+                    .forEach((el) => {
+                        el.style.opacity = "1";
+                        el.style.transform = "translateY(0)";
+                    });
+            },
+        },
     });
-  });
+});
