fbcd2bd41b76c0f05679e9770df6491b4cacab22
Almira Vasiluk <bezzbashennaya@gmail.com>
2025-08-14 11:07:07 +0300

    добавила планшетную версию с активными кнопками

--- Select.js
+++ Select.js
@@ -1,32 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper(".swiper", {
-        slidesPerView: 1.2, // По умолчанию 1 слайд
-        spaceBetween: 3.67,
+        slidesPerView: 1.2, 
         slidesPerGroup: 1,
-        centeredSlides: false, // Центрирование активного слайда
-       loop:true,
-
-        // Отступы между слайдами
-
-        // Зацикливание слайдов
-        /*autoplay: {
+        centeredSlides: false, 
+        loop: true,        
+               /*autoplay: {
           delay: 5000, // Автопрокрутка каждые 5 секунд
           disableOnInteraction: false, // Автопрокрутка не останавливается при взаимодействии
         },*/
-
 
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
         effect: 'coverflow',
-       coverflowEffect: {
-          slideShadows: true,
-          
-        modifier: 0,
-         
-          
-
+        coverflowEffect: {
+            slideShadows: true,
+            modifier: 0,
         },
         on: {
             init: function () {
@@ -41,3 +31,50 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+const button = document.querySelector('button')
+const icon = button.querySelector('.icon');
+
+const iconChange = function () {
+    const buttonOpened = button.classList.contains('showAll');
+    const buttonClosed = button.classList.contains('closeAll');
+    if (buttonOpened) {
+        button.classList.remove('showAll');
+        button.classList.add('closeAll');
+        button.textContent = 'Скрыть';
+        icon.classList.remove('icon--Show');
+        icon.classList.add('icon--Hide');
+        button.insertBefore(icon, button.firstChild);
+        console.log(icon.classList);
+
+
+    }
+    else if (buttonClosed) {
+        button.classList.remove('closeAll');
+        button.classList.add('showAll');
+        icon.classList.remove('icon--Hide');
+        icon.classList.add('icon--Show');
+        button.textContent = 'Показать все';
+        button.insertBefore(icon, button.firstChild);
+        console.log(icon.classList);
+
+
+    }
+    console.log(icon.classList);
+
+}
+
+
+button.addEventListener('click', function () {
+    const screen = window.innerWidth;
+    const slides = document.querySelectorAll('.swiper-slide');
+   const navHeight = document.querySelector('.servicesBrands')
+    if (screen > 767 && screen < 1119) {
+    iconChange();
+    for (let i = slides.length-1; i >=6; i--) {
+        const slide = slides[i];
+        navHeight.style.height='auto';
+        slide.classList.toggle('hidden');
+
+    }
+
+}});
